371ff0e7b939b49e25b199993166ae11cdedcf6a
MinhyeokBaek <minhyeokbaek100@gmail.com>
2023-07-01 22:07:58 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ const home = require("./src/routes/home") // 표시된 경로의 파일을 읽�
 //앱 세팅
 app.set("views","./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));// __dirname : 현재 app.js있는 위치를 반환 /src/public 정적경로로 추가
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
 
