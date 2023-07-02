d3b7a3d16b0e26eb505e45286b80613c62ea455c
MinhyeokBaek <minhyeokbaek100@gmail.com>
2023-07-02 13:12:13 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//자바스크립트와 연결되는 프론트 화면
 const id = document.querySelector("#id");
 const psword = document.querySelector("#psword");
 const loginBtn = document.querySelector("#loginBtn");
@@ -19,6 +19,14 @@ function login(){
         },  
         body: JSON.stringify(req), //key 값과 value가 문자열로 나옴
     })
+    .then((res) => res.json())
+    .then(console.log); //서버에서 응답하는 데이터를 받기
+    //res.json()의 반환 값은 promise다.
+    //기본 res의 반환 값은 response 스트림인데, "json()" 메서드를 통해 Response(응답) 스트림을 읽을 수 있다.
+    //Response는 데이터가 모두 받아진 상태가 아니다.
+    //".json()"으로 Response 스트림을 가져와 완료될 때까지 읽는다,
+    // 읽은 body의 텍스트를 Promise 형태로 반환한다.
+    //promise 타입은 then을 사용해서 받는다.
 
     
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const output = { //안에 렌더링되어진 내용을 객체로 빼줌
   home: (req, res) => {
     res.render("home/index")
@@ -8,11 +9,31 @@ const output = { //안에 렌더링되어진 내용을 객체로 빼줌
   },
 }
 
+const users = {
+  id: ["woorimIT", "나개발", "김팀자"],
+  psword  : ["1234", "1234", "123456"],
+}//users 데이터랑 프론트엔드에서 전달하는 body 데이터랑 인증하는 과정
+
 const process = {
   login: (req, res) => {
-    console.log(req.body); //body를 pasing 하기 위해서 app.js에 parser를 만들고 npm i body-parser -s 로 설치
-  }
-}
+    const id = req.body.id; //body를 pasing 하기 위해서 app.js에 parser를 만들고 npm i body-parser -s 로 설치
+    const psword = req.body.psword;
+
+    if(users.id.includes(id)){  //id와 psword 값이 동일한 위치로 맞추는 과정
+      const idx = users.id.indexOf(id); //includes(id)는 프론트에서 전달해주는 id가  users.id에 있으면가져온다 
+      if (users.psword[idx] === psword){  //프론트에서 가져오는 psword가 같은지 확인
+        return res.json({
+          success: true, //위에 조건이 맞으면 프론트에 응답처리
+        });
+      }
+    }
+//return으로 나왔기 때문에 else 사용을 안해도 됨
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
+  },
+};
 
 
 
