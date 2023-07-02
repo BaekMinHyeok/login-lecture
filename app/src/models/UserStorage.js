a49ae744b1d42475c1778603e1243a5422444d9f
MinhyeokBaek <minhyeokbaek100@gmail.com>
2023-07-02 14:20:29 +0900

    UserStrorage 모델 구현

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,23 @@
+"use strict";
+
+class UserStorage {
+  static #users = { //#을 붙이여야 private로 바뀌게 됨(외부에서 불러올 수 없음)
+    //static을 사용해서 정적 변수로 바꾼 후에 home.ctrl에 불러온다. 로그인버튼을 누르면 안에 값이 터미널에 표시가됨
+    id: ["woorimIT", "나개발", "김팀장"],
+    psword  : ["1234", "1234", "123456"],
+    name: ["우리밋", "나개발", "김팀장"],
+  };
+//위에 은닉화 시킨후에 메서드로 전달해줘야함
+  static getUsers(...fields) { //정적 메서드  fields라는 배열에 저장
+    const users = this.#users
+    const newUsers = fields.reduce((newUsers, fields)=>{ //newUsers에 fiedls라는 초기값이 들어감, 그 다음 변수는 newUsers옆 fields에 들어감
+      if(users.hasOwnProperty(fields)){
+        newUsers[fields] = users[fields]; //위의 키와 값을 가져온 newUsers[키] = [값]으로 생각
+      }
+      return newUsers;
+    },{}); //{}); 초기값을 오브젝트로 지정
+    return newUsers;
+  }
+} // class 안에 변수 선언은 필요없다
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const UserStorage = require("../../models/UserStorage");
+
 const output = { //안에 렌더링되어진 내용을 객체로 빼줌
   home: (req, res) => {
     res.render("home/index")
@@ -9,29 +11,27 @@ const output = { //안에 렌더링되어진 내용을 객체로 빼줌
   },
 }
 
-const users = {
-  id: ["woorimIT", "나개발", "김팀자"],
-  psword  : ["1234", "1234", "123456"],
-}//users 데이터랑 프론트엔드에서 전달하는 body 데이터랑 인증하는 과정
+//users 데이터랑 프론트엔드에서 전달하는 body 데이터랑 인증하는 과정
 
 const process = {
   login: (req, res) => {
     const id = req.body.id; //body를 pasing 하기 위해서 app.js에 parser를 만들고 npm i body-parser -s 로 설치
     const psword = req.body.psword;
 
+    const users = UserStorage.getUsers("id","psword");
+
+    const response = {};
     if(users.id.includes(id)){  //id와 psword 값이 동일한 위치로 맞추는 과정
       const idx = users.id.indexOf(id); //includes(id)는 프론트에서 전달해주는 id가  users.id에 있으면가져온다 
       if (users.psword[idx] === psword){  //프론트에서 가져오는 psword가 같은지 확인
-        return res.json({
-          success: true, //위에 조건이 맞으면 프론트에 응답처리
-        });
+        response.success = true;//위에 조건이 맞으면 프론트에 응답처리
+        return res.json(response);
       }
     }
 //return으로 나왔기 때문에 else 사용을 안해도 됨
-    return res.json({
-      success: false,
-      msg: "로그인에 실패하셨습니다.",
-    });
+    response.success = false;
+    response.msg = "로그인에 실패하셨습니다."
+    return res.json(response);
   },
 };
 
